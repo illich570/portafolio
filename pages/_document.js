d0b6223ec80a5f2001c9a3a728ace887f361c292
Illich Rada <illich570@gmail.com>
2021-10-16 14:06:17 -0400

    [FIX] correct URL of og:image of metadata

--- pages/_document.js
+++ pages/_document.js
@@ -16,9 +16,9 @@ export default class MyDocument extends Document {
 					<link href="/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
 					<link href="/site.webmanifest" rel="manifest" />
 					{/* METADATOS */}
-					<meta content="/og_image.png" name="image"/>
 					<meta content="portfolio, developer, IT, responsive, frontend, backend, programmer, programador, illich" name="keywords" />
-					<meta content="/meta.png" property="og:image"/>
+					<meta content="https://www.illichrada.com/og_image.png" name="image"/>
+					<meta content="https://www.illichrada.com/og_image.png" property="og:image"/>
 					<meta content="Illich Rada" name="author" />
   				<meta content="Illich Rada" name="copyright" />
 					<meta content={theme.palette.primary.main} name="msapplication-TileColor" />
@@ -27,12 +27,12 @@ export default class MyDocument extends Document {
             name="apple-mobile-web-app-title"
           />
           <meta content="Illich Rada | Frontend Developer" name="application-name" />
-					<meta content="Illich Rada | Frontend Develope" property="og:title"/>
+					<meta content="Illich Rada | Frontend Developer" property="og:title"/>
 					<meta
-					content="Illich Rada Frontend Developer. He Realizando proyectos con tecnologias web de alta vanguardia como React, Strapi, GraphCMS, Next.js y Gatbsy. Contactame!"
+					content="Illich Rada Frontend Developer. He Realizado proyectos con tecnologias web de alta vanguardia como React, Strapi, GraphCMS, Next.js y Gatbsy. Sientete libre de contactame!"
 					name="description"
 					/>
-					<meta content="Illich Rada Frontend Developer" property="og:description"/>
+					<meta content="Illich Rada Frontend Developer. He Realizado proyectos con tecnologias web de alta vanguardia como React, Strapi, GraphCMS, Next.js y Gatbsy. Sientete libre de contactame!" property="og:description"/>
           <meta content={theme.palette.primary.main} name="msapplication-TileColor" />
           <link crossOrigin="anonymous" href="https://fonts.gstatic.com" rel="preconnect" />
 					<link
