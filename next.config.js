96bfc9ee5dc99eb1c7ec5ff90b2cc1e6da672768
Illich Rada <illich570@gmail.com>
2023-02-01 14:05:42 -0400

    [MOD] change URL for media asset

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const { i18n } = require('./next-i18next.config')
 module.exports = {
 	i18n,
 	images: {
-		domains: ['media.graphcms.com'],
+		domains: ['media.graphassets.com'],
 	},
 	async headers() {
 		return [
